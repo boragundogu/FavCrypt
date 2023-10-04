e19232d311b312db7a0cda3e25683aadf02370ef
boragundogu <gundogubora24@gmail.com>
2023-10-04 14:25:06 +0300

    Initial Commit

---
+++ FavCrypt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FavCrypt
+//
+//  Created by Bora Gündoğu on 4.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FavCrypt/FavCryptApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavCryptApp.swift
+//  FavCrypt
+//
+//  Created by Bora Gündoğu on 4.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavCryptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
